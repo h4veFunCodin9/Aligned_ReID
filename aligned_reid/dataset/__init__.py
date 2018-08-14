91155a865c1bc48c2504ccfe3802cf68d650284f
h4veFunCodin9 <zt1330zt@163.com>
2018-08-14 19:43:39 +0800

    server changes

--- aligned_reid/dataset/__init__.py
+++ aligned_reid/dataset/__init__.py
@@ -25,8 +25,8 @@ def create_dataset(
 
   print(name)
   if name == 'market1501':
-    im_dir = ospeu('./Dataset/market1501/images')
-    partition_file = ospeu('./Dataset/market1501/partitions.pkl')
+    im_dir = ospeu('../Dataset/market1501/images')
+    partition_file = ospeu('../Dataset/market1501/partitions.pkl')
 
   elif name == 'cuhk03':
     im_type = ['detected', 'labeled'][0]
@@ -34,14 +34,14 @@ def create_dataset(
     partition_file = ospeu(ospj('~/Dataset/cuhk03', im_type, 'partitions.pkl'))
 
   elif name == 'duke':
-    im_dir = ospeu('./Dataset/duke/images')
-    partition_file = ospeu('./Dataset/duke/partitions.pkl')
+    im_dir = ospeu('../Dataset/duke/images')
+    partition_file = ospeu('../Dataset/duke/partitions.pkl')
 
   elif name == 'combined':
     assert part in ['trainval'], \
       "Only trainval part of the combined dataset is available now."
-    im_dir = ospeu('./Dataset/market1501_cuhk03_duke/trainval_images')
-    partition_file = ospeu('./Dataset/market1501_cuhk03_duke/partitions.pkl')
+    im_dir = ospeu('../Dataset/market1501_cuhk03_duke/trainval_images')
+    partition_file = ospeu('../Dataset/market1501_cuhk03_duke/partitions.pkl')
 
   ##################
   # Create Dataset #
--- aligned_reid/model/TripletLoss.py
+++ aligned_reid/model/TripletLoss.py
@@ -8,11 +8,11 @@ class TripletLoss(object):
   Loss for Person Re-Identification'."""
   def __init__(self, margin=None, margin_in=None):
     self.margin = margin
-    self.margin_in = margin_in
-    self.beta = 0.01
+    #self.margin_in = margin_in
+    #self.beta = 0.01
     if margin is not None:
       self.ranking_loss = nn.MarginRankingLoss(margin=margin)
-      self.ranking_loss_in = nn.MarginRankingLoss(margin=0)
+      #self.ranking_loss_in = nn.MarginRankingLoss(margin=0)
     else:
       self.ranking_loss = nn.SoftMarginLoss()
 
@@ -27,11 +27,11 @@ class TripletLoss(object):
       loss: pytorch Variable, with shape [1]
     """
     y = Variable(dist_an.data.new().resize_as_(dist_an.data).fill_(1))
-    in_y = Variable(dist_ap.data.new().resize_as_(dist_ap.data).fill_(1))
-    in_margins = Variable(dist_ap.data.new().resize_as_(dist_ap.data).fill_(self.margin_in))
+    #in_y = Variable(dist_ap.data.new().resize_as_(dist_ap.data).fill_(1))
+    #in_margins = Variable(dist_ap.data.new().resize_as_(dist_ap.data).fill_(self.margin_in))
     if self.margin is not None:
       loss = self.ranking_loss(dist_an, dist_ap, y)
-      loss += self.beta * self.ranking_loss_in(in_margins, dist_ap, in_y)
+      #loss += self.beta * self.ranking_loss_in(in_margins, dist_ap, in_y)
     else:
       loss = self.ranking_loss(dist_an - dist_ap, y)
     return loss
